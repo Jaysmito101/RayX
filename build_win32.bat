e103026a2df6a16332b6ffcbbfde36ab27837810
Jaysmito Mukherjee <73700725+Jaysmito101@users.noreply.github.com>
2022-01-16 16:42:58 +0530

    Added Win32 Command Line Build Util

---
+++ build_win32.bat
@@ -0,0 +1,7 @@
+@echo off
+echo Setting Up Developer Environment
+call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvarsall.bat" x86
+SET Platform=
+echo Building RayX
+msbuild /m /p:PlatformTarget=x86 /p:Configuration=Debug RayX.sln
+pause
